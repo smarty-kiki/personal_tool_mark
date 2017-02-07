579dbc927db52b7640c9b41927b79b9bc9aab543
yaoyang <yy@1mxian.com>
2017-02-07 15:53:30 +0800

    初始化 mark 项目

---
+++ bin/enable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_DIR=$SCRIPT_DIR/..
+
+function enable
+{
+    mv ~/.$1 ~/.$1.bak
+    ln -s $PROJECT_DIR/$1 ~/.$1
+}
+
+enable mark
